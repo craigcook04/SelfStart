361ca980f728a8f8c0c9e4a8425f9344a16c7295
Sam Mallabone <smallabo@uwo.ca>
2018-01-29 14:53:08 +0000

    updated some of the models to have proper types

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -2,7 +2,7 @@
 
 var express = require('express');
 var router = express.Router();
-var Exercise = require('../models/exercise');
+var Exercise = require('../models/exercises');
 
 router.route('/')
 
