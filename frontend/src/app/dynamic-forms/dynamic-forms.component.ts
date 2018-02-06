e61aa071d92e42d8f46e281b934dc1f39ec1b4ef
Matt Northmore <rnorthm@uwo.ca>
2018-02-06 23:35:15 +0000

    Continued dynamic forms

--- frontend/src/app/dynamic-forms/dynamic-forms.component.ts
+++ frontend/src/app/dynamic-forms/dynamic-forms.component.ts
@@ -1,17 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
-
-
 @Component({
   selector: 'app-dynamic-forms',
   templateUrl: './dynamic-forms.component.html',
   styleUrls: ['./dynamic-forms.component.css']
 })
 export class DynamicFormsComponent implements OnInit {
+  
+  showForm: boolean;
+  
+  
+  
+  
+  
 
   constructor() { }
 
   ngOnInit() {
+    this.showForm = false;
   }
 
 }
