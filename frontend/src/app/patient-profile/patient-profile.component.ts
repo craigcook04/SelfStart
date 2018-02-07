34ef179836d6ebb0e23ee8dc5eed58183ee5a677
Sam Mallabone <smallabo@uwo.ca>
2018-02-06 20:06:00 -0500

    Added text fields to the update modal and passing changes to be sent to the back end

--- frontend/src/app/patient-profile/patient-profile.component.ts
+++ frontend/src/app/patient-profile/patient-profile.component.ts
@@ -25,6 +25,11 @@ export class PatientProfileComponent implements OnInit {
     this.modalService.open(content);
   }
 
+  updatePatient(ID ,firstName: string, lastName: string, patientID: string, email: string, DOB: string, postalCode: string, phoneNumber: string,maritalStatus: string, healthCardNumber: string, occupation: string) {
+    console.log(ID, firstName, lastName, patientID, email, DOB, postalCode, phoneNumber, maritalStatus, healthCardNumber, occupation);
+    console.log("i printed");
+  }
+
   
 
 }
