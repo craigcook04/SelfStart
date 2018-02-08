042fb0e97cfcaa420c432a5ef9975873a1d2a232
Andrew Black <ablack65@uwo.ca>
2018-02-08 19:25:04 +0000

    Route error fixed up

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -10,13 +10,13 @@ router.route('/')
         var exercise = new Exercise.Model();
         exercise.name = request.body.name;
         exercise.description = request.body.description;
-        exercise.objectives = request.body.authorName;
+        exercise.objectives = request.body.objectives;
+        exercise.authorName = request.body.authorName;
         exercise.actionSteps = request.body.actionSteps;
         exercise.location = request.body.location;
         exercise.frequency = request.body.frequency;
         exercise.duration = request.body.duration;
-        var date = new Date(request.body.targetDate);
-        exercise.targetDate = date;
+        exercise.targetDate = request.body.targetDate;
         exercise.multimedia = request.body.multimedia;
         exercise.rehabilitationPlans = request.body.rehabilationPlans;
         
@@ -33,7 +33,7 @@ router.route('/')
         Exercise.Model.find(function (error, exercises) {
             if (error) response.send(error);
             response.json({exercise: exercises});
-        });
+        }).sort({name: 1});
     });
 
 //fetching a specific exercise
