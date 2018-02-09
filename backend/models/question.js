35ae6b156b41396ec3fcdb75f615647e0debb534
Matt Northmore <rnorthm@uwo.ca>
2018-02-09 17:43:06 +0000

    Pulling previous work to branch

--- backend/models/question.js
+++ backend/models/question.js
@@ -4,13 +4,8 @@ var questionSchema = mongoose.Schema({
     questionText: String,
     helpDescription: String,
     order: Number,
-<<<<<<< HEAD
-    form: {type: mongoose.Schema.ObjectId, ref: ('Forms')},
-    questionType: {type: mongoose.Schema.ObjectId, ref: ('QuestionType')}
-=======
     form: {type: mongoose.Schema.Types.ObjectId, ref: 'Forms'},
     questionType: {type: mongoose.Schema.Types.ObjectId, ref: 'QuestionType'}
->>>>>>> 6f4ef527b64e09d8f1be77fc7a65c914e81350d0
 });
 
 var Question = mongoose.model('Question', questionSchema);
