97fd2d8c49c408b6277f36943ef3c3f11378c5fe
Jak <jterpak@uwo.ca>
2018-02-22 03:41:22 +0000

    working on user accounts, new acc view

--- frontend/src/app/user-accounts/user-accounts.component.ts
+++ frontend/src/app/user-accounts/user-accounts.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { PatientService } from '../patient.service';
 import { UserAccountsService } from '../user-accounts.service';
 import {RehabPlansService} from '../rehab-plans.service';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-user-accounts',
@@ -12,12 +13,14 @@ export class UserAccountsComponent implements OnInit {
 
   constructor(private patientService: PatientService,
               private userAccountService: UserAccountsService,
-              private rehabPlansService: RehabPlansService) { }
+              private rehabPlansService: RehabPlansService,
+              private modalService: NgbModal) { }
   
   clients: Object[];
   therapists: Object[];
   content: boolean;
   activated: any;
+  genders: Object[];
   
   ngOnInit() {
     this.content = false;
@@ -25,6 +28,10 @@ export class UserAccountsComponent implements OnInit {
       this.clients = Object.assign([], data.patients);
       console.log('hello');
       // console.log(this.patients);
+    });
+     this.patientService.GetGenders().subscribe(data => {
+      var retObj: any = data;
+      this.genders = Object.assign([], retObj.gender);
     });
   }
   show(client: any){
@@ -38,5 +45,54 @@ export class UserAccountsComponent implements OnInit {
     console.log(this.activated);
     
   }
+   open(content) {
+    this.modalService.open(content, {size: 'lg'});
+  }
+   updatePatient(ID: string, firstName: string, lastName: string, patientID: string, email: string, DOB: string, postalCode: string, phoneNumber: string, maritalStatus: string, healthCardNumber: string, occupation: string, others: string, newCountry: string, newProvince: string, newCity: string, newGender: string) {
+    
+   //this.showSuccess = true;
+    this.patientService.UpdatePatient(ID, firstName, lastName, patientID, email, DOB, postalCode, phoneNumber, maritalStatus, healthCardNumber, occupation, others, newCountry, newProvince, newCity, newGender).subscribe(data => {
+      console.log(data);
+      //reload the list of patients
+      this.patientService.GetAllPatients().subscribe(data => {
+        this.clients = Object.assign([], data.patients);
+        console.log(data);
+      });
+      this.activated = null;
+      if(data.success) {
+        //the update was successful
+        //this.showSuccess = true;
+      }
+      else{
+        //it was not successfuls
+        //this.showFailure = true;
+      }
+    })
+
+  }
+
+  deletePatient(ID: string) {
+    console.log(ID);
+    this.patientService.DeletePatient(ID).subscribe(data => {
+      var retObj: any = data;
+      if(retObj.success){
+        // this.showDeleteSuccess = true;
+        // this.showSuccess = false;
+        // this.showFailure = false;
+        // this.showCreationSuccess = false;
+        this.activated = null;
+        //acc.activeIds = []; //close all accordian panels
+        this.patientService.GetAllPatients().subscribe(data => {
+          this.clients = Object.assign([], data.patients);
+        });
+      }
+      else { 
+       // this.showFailure = true;
+      }
+      
+    })
+  }
+
+
 
 }
