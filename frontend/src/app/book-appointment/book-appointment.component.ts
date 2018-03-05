dd77cbb116213c26c4fb4f6f815321672d88e9b5
ablack65 <ablack65@uwo.ca>
2018-03-05 08:51:56 -0500

    Before Getting Matt's Branch

--- frontend/src/app/book-appointment/book-appointment.component.ts
+++ frontend/src/app/book-appointment/book-appointment.component.ts
@@ -21,6 +21,7 @@ export class BookAppointmentComponent implements OnInit {
   public uploader:FileUploader = new FileUploader({url: URL});
 
   model: NgbDateStruct;
+  invalidName: boolean = false;
 
   @ViewChild('dp') dp: NgbDatepicker;
 
@@ -38,6 +39,37 @@ export class BookAppointmentComponent implements OnInit {
 
   selectToday(){
     this.model = {year: now.getFullYear(), month: now.getMonth() + 1, day: now.getDate()};
+  }
+
+  determineAge(){
+    console.log("HERE");
+    var age = document.querySelector('dp').innerHTML;
+    console.log(age);
+  }
+
+  sendInitialInfoSheet(makeChanges){
+
+    var name: any = document.querySelector('inputName');
+    name = name.value;
+
+    //regex commands for input validation
+    var badFormat = /[ !\s\t@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/; //regex statement to limit bad characters in a username
+    var badFormatWithNumbers =  /[ !\s\t@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?\d]/ //regex format to confirm input of first name and last name
+    var badFormatWithLetters = /[ !\s\t@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/
+    var emailFormat =  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    var validPhoneNumber = /^\(?([0-9]{3})\)?[-.●]?([0-9]{3})[-.●]?([0-9]{4})$/
+    var cannotContinue: boolean = false; //if there are any errors in the form this stops from sending the request from the server
+
+    if(badFormat.test(name) || !name){
+      var nameBox = document.getElementById('inputName').style.borderColor = "red";
+      this.invalidName = true;
+      cannotContinue = true;
+    }
+
+    if(cannotContinue){
+      this.modalService.open(makeChanges, {size: 'lg'});
+      return;
+    }
   }
 
 
