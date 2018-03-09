e3424ff3cb616ad77372293ea73ab743068cffb0
Sam Mallabone <smallabo@uwo.ca>
2018-03-09 19:40:43 +0000

    Added in a new schema for resetting a user's password

--- backend/routes/email.js
+++ backend/routes/email.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var router = express.Router();
 var nodemailer = require("nodemailer");
+var ResetEmail = require('./models/resetEmail');
 
 var smtpTransport = nodemailer.createTransport({
     service: "Gmail",
@@ -10,6 +11,16 @@ var smtpTransport = nodemailer.createTransport({
         pass: "Selfstart"
     } 
 });
+
+function makeHash() {
+  var text = "";
+  var possible = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < 25; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+}
 
 
 router.route('/')
@@ -33,9 +44,14 @@ router.route('/')
     
 router.route('/forgotten')
     .post(function(request, response) {
+       var resetEmail = new ResetEmail();
+       resetEmail.username = request.body.username;
+       var randomHash = makeHash();
+       resetEmail.myHash = randomHash;
+       resetEmail.myDate = new Date();
        var emailBody = `
        <h4>Hello, please click the link below to reset your password </h4>
-       <p><a href="localhost:8080/forgottenpassword"
+       <p><a href="localhost:8080/"
        `
        var mailOptions = {
            
