9ec96e503b244cbf7ab301c936b4b3d48666438e
ablack65 <ablack65@uwo.ca>
2018-03-13 12:59:37 -0400

    Starting Client Exercises

---
+++ frontend/src/app/client-exercise/client-exercise.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-client-exercise',
+  templateUrl: './client-exercise.component.html',
+  styleUrls: ['./client-exercise.component.css']
+})
+export class ClientExerciseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
