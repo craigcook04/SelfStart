35ef0b3f69be6125c6f6ddb7918f442ff4bd9a25
Jak <jterpak@uwo.ca>
2018-03-13 19:42:17 +0000

    changed some schemas

--- backend/models/assessmentTest.js
+++ backend/models/assessmentTest.js
@@ -3,10 +3,15 @@ var mongoose = require("mongoose");
 var assessmentTestSchema = new mongoose.Schema({
     name: String,
     description: String,
-    authorName: String,
-    recommendation: [{type: mongoose.Schema.Types.ObjectId, ref: 'Recommendation'}],
-    testResults: [{type: mongoose.Schema.Types.ObjectId, ref: 'TestResult'}],
-    rehabilitionPlans: [{type: mongoose.Schema.Types.ObjectId, ref: 'RehabilitionPlans'}]
+    completed: Boolean,
+    creator: {type: mongoose.Schema.Types.ObjectId, ref: 'Physiotherapist'},
+    belongsTo: {type: mongoose.Schema.Types.ObjectId, ref: 'Patient'}
+    
+    
+    // authorName: String,
+    // recommendation: [{type: mongoose.Schema.Types.ObjectId, ref: 'Recommendation'}],
+    // testResults: [{type: mongoose.Schema.Types.ObjectId, ref: 'TestResult'}],
+    // rehabilitionPlans: [{type: mongoose.Schema.Types.ObjectId, ref: 'RehabilitionPlans'}]
 });
 
 var AssessmentTest = mongoose.model('AssessmentTest', assessmentTestSchema);
