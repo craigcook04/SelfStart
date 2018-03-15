886db807c0d0b287f34f0abb6a2273a263fd4540
Matt Northmore <rnorthm@uwo.ca>
2018-03-15 02:44:54 +0000

    User can select apointment times and cancel selections

--- frontend/src/app/appointments/appointments.component.ts
+++ frontend/src/app/appointments/appointments.component.ts
@@ -20,6 +20,7 @@ export class AppointmentsComponent implements OnInit, AfterViewInit {
   isSelected;
   bookedDates: any[];
   timeIndex: any; 
+  dayIndex: any;
 
   constructor(private modalService: NgbModal,
               private router: Router,
@@ -39,30 +40,46 @@ export class AppointmentsComponent implements OnInit, AfterViewInit {
       console.log(this.bookedDates);
       
       for(var i = 0; i < this.bookedDates.length; i++ ){
-        if(moment(this.bookedDates[i].date).isSameOrAfter(moment().add(this.currentWeek, 'weeks').startOf('week').format('YYYY-MM-DD')) && moment(this.bookedDates[i].date).isSameOrBefore(moment().add(this.currentWeek, 'weeks').endOf('week').format('YYYY-MM-DD'))){
+        if(moment(this.bookedDates[i].date).isSameOrAfter(moment().add(this.currentWeek, 'weeks').startOf('week')) && moment(this.bookedDates[i].date).isSameOrBefore(moment().add(this.currentWeek, 'weeks').startOf('week').add(7, 'days'))){
           var currDate = moment(this.bookedDates[i].date) //creates moment object out of string date
           var dayNum = currDate.day(); //this gets the day of the week in numerical form 0 = sunday -> 6=saturday
-          var tempTime  = moment().add(this.currentWeek, 'weeks').startOf('week').add(dayNum, 'days').startOf('day').add(8, 'hours').add(30, 'minutes').format('YYYY-MM-DD'); //get start of selected week
+          var tempTime  = moment().add(this.currentWeek, 'weeks').startOf('week').add(dayNum, 'days').startOf('day').add(8.5, 'hours'); //get start of selected day
           var timeCount = 0;
+          var nextSlot = 0;
+          var secondSlot = 0;
           var countUp = true;
           //figure out how many 15-minute intervals are needed to get to time
           //** the work day starts at 830
           
-          while(countUp){
+          while(true){
             if(moment().add(this.currentWeek, 'weeks').startOf('week').add(dayNum, 'days').startOf('day').add(8, 'hours').add((30 + (timeCount*15)), 'minutes').isBefore(currDate)){
-              timeCount = timeCount + 1; //will give me slot number
               
+              timeCount = timeCount + 1; //will give me slot number
+              nextSlot = timeCount + 1;
+              secondSlot = nextSlot + 1;
             }
             else{
-              countUp = false;
+              break;
             }
           }
           
           console.log("made it here " + timeCount);
-          var takenSlot = "slot" + (dayNum + 1) + timeCount; //slot0{{i}}, slot1{{i}} ...etc
-          // document.getElementById(takenSlot).classList.remove('bg-primary');
-          document.getElementById(takenSlot).setAttribute("class", "btn btn-sm bg-muted chooseTime");
-          console.log(dayNum);
+          var takenSlot = [("slot" + dayNum + timeCount), ("slot" + dayNum + nextSlot), ("slot" + dayNum + secondSlot)]; //slot0{{i}}, slot1{{i}} ...etc
+          
+          if(timeCount > 34){
+            if(timeCount == 35){
+              document.getElementById(takenSlot[0]).setAttribute("class", "btn btn-sm bg-muted chooseTime");
+              document.getElementById(takenSlot[1]).setAttribute("class", "btn btn-sm bg-muted chooseTime");
+            }else{
+              document.getElementById(takenSlot[0]).setAttribute("class", "btn btn-sm bg-muted chooseTime");
+            }
+          }else{
+            document.getElementById(takenSlot[0]).setAttribute("class", "btn btn-sm bg-muted chooseTime");
+            document.getElementById(takenSlot[1]).setAttribute("class", "btn btn-sm bg-muted chooseTime");
+            document.getElementById(takenSlot[2]).setAttribute("class", "btn btn-sm bg-muted chooseTime");
+          }
+          
+          //console.log(dayNum);
         }
       }
     });
@@ -74,12 +91,53 @@ export class AppointmentsComponent implements OnInit, AfterViewInit {
   choosenSlot(day: any, indx: any){ //day is hard coded, index isnt
     this.isSelected = true; //If one is selected they cant select another
     this.timeIndex = indx; //keeping variable with selected index
-    day = Number(day) - 1;
+    this.dayIndex = day;
+    // day = Number(day) - 1;
     this.currentlyHighlighted = moment().startOf('week').startOf('day').add(this.currentWeek, 'weeks').add((day), 'days').add(8.5, 'hours').add((this.timeIndex*15), 'minutes');
-    console.log(this.currentlyHighlighted);
-    this.apptService.AddAppointment(this.currentlyHighlighted, 'test69', 'test69').subscribe(data =>{
-      console.log(data);
-    });
+    //console.log(this.currentlyHighlighted);
+    var highlighted = [("slot"+day+indx), ("slot"+day+(indx+1)), ("slot"+day+(indx+2))];
+    
+    if(indx > 34){
+      if(indx == 35){
+        document.getElementById(highlighted[0]).setAttribute("class", "btn btn-sm bg-warning chooseTime");
+        document.getElementById(highlighted[1]).setAttribute("class", "btn btn-sm bg-warning chooseTime");
+      }else{
+        document.getElementById(highlighted[0]).setAttribute("class", "btn btn-sm bg-warning chooseTime");
+      }
+    }else{
+      document.getElementById(highlighted[0]).setAttribute("class", "btn btn-sm bg-warning chooseTime");
+      document.getElementById(highlighted[1]).setAttribute("class", "btn btn-sm bg-warning chooseTime");
+      document.getElementById(highlighted[2]).setAttribute("class", "btn btn-sm bg-warning chooseTime");
+    }
+    
+  }
+  
+  cancelSelection(){
+    
+    var highlighted = [("slot"+this.dayIndex+this.timeIndex), ("slot"+this.dayIndex+(this.timeIndex+1)), ("slot"+this.dayIndex+(this.timeIndex+2))];
+    
+    if(this.timeIndex > 34){
+      if(this.timeIndex == 35){
+        document.getElementById(highlighted[0]).setAttribute("class", "btn btn-sm bg-primary chooseTime");
+        document.getElementById(highlighted[1]).setAttribute("class", "btn btn-sm bg-primary chooseTime");
+      }else{
+        document.getElementById(highlighted[0]).setAttribute("class", "btn btn-sm bg-primary chooseTime");
+      }
+    }else{
+      document.getElementById(highlighted[0]).setAttribute("class", "btn btn-sm bg-primary chooseTime");
+      document.getElementById(highlighted[1]).setAttribute("class", "btn btn-sm bg-primary chooseTime");
+      document.getElementById(highlighted[2]).setAttribute("class", "btn btn-sm bg-primary chooseTime");
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
   }
   
   refreshCalendar(){
@@ -116,5 +174,13 @@ export class AppointmentsComponent implements OnInit, AfterViewInit {
     this.apptService.AddAppointment(date, reason, other).subscribe(data =>{
       console.log(data);
     })
+  }
+  
+  
+  //this is the function ive made for submitting a selection
+  saveAppointment(){
+    this.apptService.AddAppointment(this.currentlyHighlighted, 'test69', 'test69').subscribe(data =>{
+      console.log(data);
+    });
   }
 }
