ebef61955dfa1653ac1b8f7af748a3a76c8aebd8
Stephanie Pereira <sperei32@uwo.ca>
2018-03-23 07:12:52 +0000

    nav

--- frontend/src/app/physio-home/physio-home.component.ts
+++ frontend/src/app/physio-home/physio-home.component.ts
@@ -114,7 +114,7 @@ export class PhysioHomeComponent implements OnInit {
     this.router.navigate(['../rehabplans']);
   }
   goToTests(){
-    
+    this.router.navigate(['../assessmentTest']);
   }
   goToReports(){
     
