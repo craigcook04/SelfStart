e62635798527a4c2d8c31e6440773979385c66f5
Stephanie Pereira <sperei32@uwo.ca>
2018-03-23 07:04:48 +0000

    add events

--- frontend/src/app/physio-home/physio-home.component.ts
+++ frontend/src/app/physio-home/physio-home.component.ts
@@ -3,6 +3,27 @@ import { Router } from '@angular/router';
 import { PhysioHomeService } from '../physio-home.service';
 import { CalendarEvent } from 'angular-calendar';
 import { ViewEncapsulation } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { colors } from '../../demo-utils/colors';
+import { map } from 'rxjs/operators/map';
+import {
+  isSameMonth,
+  isSameDay,
+  startOfMonth,
+  endOfMonth,
+  startOfWeek,
+  endOfWeek,
+  startOfDay,
+  endOfDay,
+  format
+} from 'date-fns';
+
+interface Appointment {
+  //date: date;
+  name: string;
+  reason: string;
+  other: string;
+}
 
 @Component({
   selector: 'app-physio-home',
@@ -12,17 +33,24 @@ import { ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None,
   providers: [PhysioHomeService]
 })
+
 export class PhysioHomeComponent implements OnInit {
 
   constructor(private router: Router, private physioHomeService: PhysioHomeService) { }
   
-  view: string = 'month';
-  viewDate: Date = new Date();
-  events: CalendarEvent[] = [];
-  clickedDate: Date;
-  
   activated: any;
   appointments: any[];
+  
+  view: string = 'month';
+  viewDate: Date = new Date();
+  //events: CalendarEvent[] = [];
+  //clickedDate: Date;
+  events$: Observable<Array<CalendarEvent<{ appointment: Appointment }>>>;
+  activeDayIsOpen: boolean = false;
+
+  
+  //events: any;
+  //items: Array<CalendarEvent<{ time: any }>> = [];
   
   ngOnInit() {
     this.appointments = [];
@@ -31,8 +59,41 @@ export class PhysioHomeComponent implements OnInit {
       var retObj:any = data;
       this.appointments = retObj.appointment;
       console.log(this.appointments);
+        /*for(let i=0; i<res.data.length; i++) {
+          this.items.push(
+          {
+            title: res.data[i].name,
+            start: new Date(res.data[i].date),
+            color: {colors.blue},
+            meta: {
+              time: res.data[i].date
+            }
+          });
+          this.events = this.items;
+        }*/
     });
   }
+  
+  dayClicked({
+    date,
+    events
+  }: {
+    date: Date;
+    events: Array<CalendarEvent<{ appointment: Appointment }>>;
+  }): void {
+    if (isSameMonth(date, this.viewDate)) {
+      if (
+        (isSameDay(this.viewDate, date) && this.activeDayIsOpen === true) ||
+        events.length === 0
+      ) {
+        this.activeDayIsOpen = false;
+      } else {
+        this.activeDayIsOpen = true;
+        this.viewDate = date;
+      }
+    }
+  }
+  
   show(appointment: any){
     if(this.activated == appointment){
       this.activated = null;
