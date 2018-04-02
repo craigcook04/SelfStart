0ac8a4a759bd1d3d5ac6b2eed66b56c33461f1d6
Sam Mallabone <smallabo@uwo.ca>
2018-04-02 14:12:10 -0400

    saving front end changes

--- frontend/src/app/generate-report/generate-report.component.ts
+++ frontend/src/app/generate-report/generate-report.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { PatientService } from '../patient.service';
+import { ActivatedRoute, Router} from '@angular/router'
 @Component({
   selector: 'app-generate-report',
   templateUrl: './generate-report.component.html',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GenerateReportComponent implements OnInit {
 
-  constructor() { }
+  patient: any;
+  constructor(private patientService: PatientService,
+              private activatedRoute: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
+    var patientID = this.activatedRoute.snapshot.paramMap.get("id");
+    this.patientService.GetPatientByPatientID(patientID).subscribe(data => {
+      var retObj: any = data;
+      console.log(data);
+      this.patient = retObj.patient;
+    })
   }
 
 }
